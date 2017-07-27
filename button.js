a342f482b698afb62147bb45f01a9c7a1062eae5
Jake Mensch <jake.mensch@gmail.com>
2017-07-26 19:36:22 -0700

    improved insights dropdown button

--- button.js
+++ button.js
@@ -1,40 +1,51 @@
 
 (function($) {
 
-  $(document).ready(function() {
+  /////////// ADD MENU ITEM //////////
 
-    var url = encodeURIComponent(window.location.pathname.substring(1));
-    console.log("repo url:", url);
+  function addMenuItem() {
 
-    // add button to clone window
-    var newButton = $(
-      '<a ' + 
-        'href="https://www.google.com/?repo=' + url + '"' + 
-        'target="_blank" ' + 
-        'class="btn btn-outline get-repo-btn">' + 
-        'Visualize on codeflower.la' + 
-      '</a>'
-    ).css({
-      'float': 'none',
-      'width': '100%'
-    });
+    var author = $('[itemprop="author"] a').text(),
+        name   = $('[itemprop="name"]   a').text();
 
-    $('.get-repo-btn:last-child').after(newButton);
+    console.log("author:", author);
+    console.log("name:", name);
+
+    if (!author || !name)
+      return false;
+
+    var url = "http://localhost:3000/?repo=" + encodeURIComponent(author + '/' + name);
+
+    var octicon = (
+      '<svg aria-hidden="true" class="octicon octicon-telescope" height="16" version="1.1" viewBox="0 0 12 16" width="12">' + 
+        '<path fill-rule="evenodd" d="M8 9l3 6h-1l-2-4v5H7v-6l-2 5H4l2-5 2-1zM7 0H6v1h1V0zM5 3H4v1h1V3zM2 1H1v1h1V1zM.63 9a.52.52 0 0 0-.16.67l.55.92c.13.23.41.31.64.2l1.39-.66-1.16-2-1.27.86.01.01zm7.89-5.39l-5.8 3.95L3.95 9.7l6.33-3.03-1.77-3.06h.01zm4.22 1.28l-1.47-2.52a.51.51 0 0 0-.72-.17l-1.2.83 1.84 3.2 1.33-.64c.27-.13.36-.44.22-.7z"></path>' + 
+      '</svg>'
+    );
 
     // add link to Insights dropdown
-    var btn = $('.reponav-dropdown');
-
     var newLink = $(
       '<a ' + 
         'class="dropdown-item" ' + 
-        'href="https://www.google.com/?repo=' + url + '">' + 
-        'Codeflower' + 
+        'href="' + url + '" ' + 
+        'target="_blank" ' + 
+        'data-skip-pjax>' + 
+        octicon + 
+        '&nbsp;Codeflower' + 
       '</a>'
     );
 
-    btn.find('.dropdown-item').last().after(newLink);
+    $('.reponav-dropdown .dropdown-item:last-child').after(newLink);
 
-  });
+  }
+
+  ////////// EVENT LISTENERS ///////////
+
+  // add menu item initial load
+  $(document).ready(addMenuItem);
+
+  // re-add menu item on tab changes 
+  // see https://github.com/defunkt/jquery-pjax
+  $(document).on('pjax:complete', addMenuItem);
 
 })(jQuery);
 
