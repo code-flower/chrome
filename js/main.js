eb1cb04ac9535d901cd7cfd2fd163b4392e65adb
Jake Mensch <jake.mensch@gmail.com>
2020-06-28 16:11:56 -0700

    fixed nav button to account for new github UI

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
 
   // determine whether this is a page when we should run the code
   function extensionActive() {
-    return !!($('.reponav')[0]);
+    return !!($('.js-repo-nav')[0]);
   }
 
   // determine what repo/branch is on the page
@@ -36,31 +36,50 @@
 
     // the icon
     var octicon = (
-      '<svg aria-hidden="true" class="octicon octicon-telescope" height="16" version="1.1" viewBox="0 0 12 16" width="12">' +
+      '<svg height="16" class="octicon octicon-telescope UnderlineNav-octicon d-none d-sm-inline" class_names="UnderlineNav-octicon" display="none inline" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true">' +
         '<path fill-rule="evenodd" d="M8 9l3 6h-1l-2-4v5H7v-6l-2 5H4l2-5 2-1zM7 0H6v1h1V0zM5 3H4v1h1V3zM2 1H1v1h1V1zM.63 9a.52.52 0 0 0-.16.67l.55.92c.13.23.41.31.64.2l1.39-.66-1.16-2-1.27.86.01.01zm7.89-5.39l-5.8 3.95L3.95 9.7l6.33-3.03-1.77-3.06h.01zm4.22 1.28l-1.47-2.52a.51.51 0 0 0-.72-.17l-1.2.83 1.84 3.2 1.33-.64c.27-.13.36-.44.22-.7z"></path>' +
       '</svg>'
     );
 
-    // add tab to navigation bar
-    $('.reponav').append(
-      '<a ' +
-        'id="codeflower-init" ' +
-        'class="reponav-item" ' +
-        'href="#">' +
-        octicon +
-        '&nbsp;Codeflower' +
-      '</a>'
+    // add codeflower to nav menu
+    $('.js-repo-nav ul.UnderlineNav-body').append(
+      '<li class="d-flex">' +
+        '<a ' +
+          'class="codeflower-init js-selected-navigation-item UnderlineNav-item hx_underlinenav-item no-wrap js-responsive-underlinenav-item" ' +
+          'data-tab-item="codeflower-tab" ' +
+          'href="#">' +
+          octicon +
+          '<span data-content="Codeflower">' +
+            'Codeflower' +
+          '</span>' +
+        '</a>' +
+      '</li>'
+    );
+
+    // add codeflower to dropdown menu
+    $('.js-repo-nav .dropdown-menu ul').append(
+      '<li data-menu-item="codeflower-tab">' +
+        '<a role="menuitem" class="codeflower-init js-selected-navigation-item dropdown-item" href="#">' +
+          'Codeflower' +
+        '</a>' +
+      '</li>'
     );
 
     // add click handler
-    $('#codeflower-init').click(function(e) {
+    $('.codeflower-init').click(function(e) {
 
       // don't follow the link
       e.preventDefault();
 
-      // select the menu item
-      $('.reponav-item').removeClass('selected');
-      $('#codeflower-init').addClass('selected');
+      // unselect other menu items
+      $('.js-selected-navigation-item')
+        .removeClass('selected')
+        .removeAttr('aria-current');
+
+      // select the codeflower menu item
+      $('.codeflower-init:not(.dropdown-item)')
+        .addClass('selected')
+        .attr('aria-current', 'page');
 
       // inject codeflower into the DOM
       injectCodeflower();
@@ -128,5 +147,3 @@
   });
 
 })(jQuery);
-
-
